1dc105256fcf61111a3928ef0bca63400b6909af
Prakash Mishra <ji676349@gmail.com>
2026-03-13 00:04:50 +0530

    update

--- website/next.config.ts
+++ website/next.config.ts
@@ -2,9 +2,13 @@ import type { NextConfig } from "next";
 
 const nextConfig: NextConfig = {
   /* config options here */
+  output: 'export', // Yeh add karna zaroori hai Static Site ke liye
   reactCompiler: true,
   reactStrictMode: true,
   outputFileTracingRoot: process.cwd(),
+  images: {
+    unoptimized: true, // Static export mein images ke liye yeh chahiye hota hai
+  },
 };
 
 export default nextConfig;
